47ec611bd5d1ec6ff2402898567c7bdd9915585e
yngliu <yngliu@vmware.com>
2015-12-20 10:58:52 +0800

    Fix TestConfig conflicts issue.

--- core/src/test/java/com/sequenceiq/cloudbreak/service/messages/CloudbreakMessagesServiceTest.java
+++ core/src/test/java/com/sequenceiq/cloudbreak/service/messages/CloudbreakMessagesServiceTest.java
@@ -11,7 +11,7 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = { MessagesConfig.class, TestConfig.class })
+@ContextConfiguration(classes = { MessagesConfig.class, MsgTestConfig.class })
 public class CloudbreakMessagesServiceTest {
 
     @Inject
